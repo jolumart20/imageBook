dc4c6fa08f441dc5f585fa6db44904fa988b5d97
Ram Muñoz <raalmunozla@ittepic.edu.mx>
2025-06-02 22:01:44 -0700

    Ultima actualizacion de Ramsisus
    Pos ya que mas se hace vea

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 config();
 
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://jolumartinezro:57FdWQteNdhdgrpJ@imagebook.1dfzggp.mongodb.net/?retryWrites=true&w=majority&appName=imageBook";
-export const PORT = process.env.PORT || 3001;
+export const PORT = process.env.PORT || 3000;
